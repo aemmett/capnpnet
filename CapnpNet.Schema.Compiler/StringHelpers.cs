d56c7b90b4b715facc2a02a476d03e3075981cf9
Alec Emmett <_@_>
2018-01-14 17:56:02 -0500

    Fix release builds; add missing file

---
+++ CapnpNet.Schema.Compiler/StringHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CapnpNet.Schema.Compiler
+{
+  internal static class StringHelpers
+  {
+    public static string StringJoin(this IEnumerable<string> parts, string separator)
+    {
+      return string.Join(separator, parts);
+    }
+  }
+}
